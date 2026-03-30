8f375f1682dd3bbdd7e26642c4d3187e597a6db0
Wolfieelite <martetaylor333@gmail.com>
2026-03-29 22:37:08 -0700

    Added navigation links to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,21 +4,28 @@ import { Button } from "./ui/button";
 
 export default function Header() {
   return (
-    <header className="border-b-2">
-      <ul className="flex flex-row justify-between">
-        <li>
-          <Link to="/">
-            <img src={imageSVG.mainLogo.src} alt="The better advisor logo" />
+    <header className="border-b-2 flex justify-between items-center">
+      <Link to="/">
+        <img src={imageSVG.mainLogo.src} width={120} height={120} alt="The better advisor logo" />
+      </Link>
+
+      <ul className="flex gap-3 h-full bg-red-50">
+        <li className="">
+          {/*Create the enroll route path to fix error*/}
+          <Link to="/enroll">
+            <Button variant={"secondary"}>Enroll</Button>
           </Link>
         </li>
 
-        <li className="flex gap-3">
-          {/*Create the enroll route path to fix error*/}
-          <Link to="/enroll">
-            <Button variant={"default"}>Enroll</Button>
-          </Link>
+        <li>
           <Link to="/login">
-            <Button variant={"secondary"}>login</Button>
+            <Button variant={"secondary"}>Student portal</Button>
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/search">
+            <Button variant={"secondary"}>Search classes</Button>
           </Link>
         </li>
       </ul>
