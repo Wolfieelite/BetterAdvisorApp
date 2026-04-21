7583011114f8e251fc62bd90f21e1f6ed9f6c2ee
Wolfieelite <martetaylor333@gmail.com>
2026-04-20 21:00:13 -0700

    updated FormElement: Fieldgroup wrapping every compoent

--- src/components/FormElement.tsx
+++ src/components/FormElement.tsx
@@ -1,9 +1,9 @@
-import { Field, FieldGroup, FieldLabel, FieldError } from '#/components/ui/field'
+import { Field, FieldLabel, FieldError } from '#/components/ui/field'
 import { Controller } from 'react-hook-form';
 import type { UseFormReturn, FieldValues, Path } from 'react-hook-form';
 import { Input } from './ui/input';
 
-type InputType = "email" | "password" | "date"
+type InputType = "text" | "email" | "password" | "date" | "tel" | "checkbox"
 
 interface FormElementProps<T extends FieldValues> {
   form: UseFormReturn<T>;
@@ -15,24 +15,22 @@ interface FormElementProps<T extends FieldValues> {
 
 function FormElement<T extends FieldValues>({ form, inputType, name, htmlForId, label }: FormElementProps<T>) {
   return (
-    <FieldGroup>
-      <Controller
-        name={name}
-        control={form.control}
-        render={({ field, fieldState }) => (
-          <Field data-invalid={fieldState.invalid}>
-            <FieldLabel htmlFor={htmlForId}>{label}</FieldLabel>
-            <Input
-              className={fieldState.invalid ? 'border-red-400 outline-0 aria-invalid:ring-red-400' : ' '}
-              type={inputType}
-              {...field}
-              id={htmlForId}
-              aria-invalid={fieldState.invalid} />
-            {fieldState.invalid && (<FieldError errors={[fieldState.error]} />)}
-          </Field>
-        )}
-      />
-    </FieldGroup>
+    <Controller
+      name={name}
+      control={form.control}
+      render={({ field, fieldState }) => (
+        <Field data-invalid={fieldState.invalid}>
+          <FieldLabel htmlFor={htmlForId}>{label}</FieldLabel>
+          <Input
+            className={fieldState.invalid ? 'border-red-400 outline-0 aria-invalid:ring-red-400' : ' '}
+            type={inputType}
+            {...field}
+            id={htmlForId}
+            aria-invalid={fieldState.invalid} />
+          {fieldState.invalid && (<FieldError errors={[fieldState.error]} />)}
+        </Field>
+      )}
+    />
   )
 }
 
