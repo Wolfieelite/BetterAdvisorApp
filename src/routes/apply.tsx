155577427f7004277f5b312dd80823c88f9f7ead
Wolfieelite <martetaylor333@gmail.com>
2026-04-20 21:00:45 -0700

    Created enrolling form

--- src/routes/apply.tsx
+++ src/routes/apply.tsx
@@ -1,9 +1,40 @@
+import FormElement from '#/components/FormElement';
+import { Button } from '#/components/ui/button';
+import { FieldGroup } from '#/components/ui/field';
+import { applySchema } from '#/schema/application.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { createFileRoute } from '@tanstack/react-router'
+import { useForm } from 'react-hook-form'
+import z from 'zod';
 
 export const Route = createFileRoute('/apply')({
   component: RouteComponent,
 })
 
+
 function RouteComponent() {
-  return <div>Hello "/apply"!</div>
+  const form = useForm<z.infer<typeof applySchema>>({
+    resolver: zodResolver(applySchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    }
+  })
+
+  return (
+    <>
+      <form className='p-12' id='applyForm'>
+        <h1>Apply at the college of the redwoods</h1>
+        <hr />
+        <FieldGroup >
+          <FormElement form={form} label='First name:' htmlForId='firstname' inputType='text' name='firstName' />
+          <FormElement form={form} label='Last name' htmlForId='lastName' inputType='text' name='lastName' />
+          <FormElement form={form} label='Email' htmlForId='email' inputType='email' name='email' />
+          <Button className="cursor-pointer" type={"submit"} variant={"default"} form={"applyForm"}>Apply</Button>
+        </FieldGroup>
+      </form>
+    </>
+  )
 }
+
---
+++ src/schema/application.schema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+
+export const applySchema = z.object({
+  // Personal Info
+  firstName: z
+    .string()
+    .min(1, "First name is required")
+    .max(50, "First name cannot exceed 50 characters")
+    .regex(/^[a-zA-Z\s'-]+$/, "First name contains invalid characters"),
+
+  lastName: z
+    .string()
+    .min(1, "Last name is required")
+    .max(50, "Last name cannot exceed 50 characters")
+    .regex(/^[a-zA-Z\s'-]+$/, "Last name contains invalid characters"),
+
+  email: z
+    .string()
+    .min(1, "Email is required")
+    .email("invalid email address"),
+})
+export type applySchema = z.infer<typeof applySchema>;
